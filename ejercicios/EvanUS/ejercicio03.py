5999197103211ff1200b3666f37f70e85820bdc5
Evan Umaña <evanumanasojo@gmail.com>
2026-06-07 13:23:37 -0600

    Realizado Ejercicio 3 Estudiante 1

--- ejercicios/EvanUS/ejercicio03.py
+++ ejercicios/EvanUS/ejercicio03.py
@@ -1,12 +1,10 @@
-def mayor_lista(lista, res = 0, isfirst=True):
+def mayor_lista(lista, res=None, isfirst=True):
     if lista == [] and isfirst:
         return 'La lista no tenia elementos inicialmente.'
     elif lista == []:
         return res
     else:
-        return mayor_lista(lista[1:], res=lista[0], isfirst=False) if lista[0] > res else mayor_lista(lista[1:], res, isfirst=False)
-    
-print(mayor_lista([4, 8, 1, 9, 3]))
-print(mayor_lista([10, 2, 5, 7]))
-print(mayor_lista([-3, -8, -1, -10]))
-print(mayor_lista([6]))
+        if isfirst:
+            return mayor_lista(lista[1:], res=lista[0], isfirst=False)
+        else:
+            return mayor_lista(lista[1:], res=lista[0], isfirst=False) if lista[0] > res else mayor_lista(lista[1:], res, isfirst=False)

c3345d6daedfd75cf4281ecfcd6737a195e3658a
Evan Umaña <evanumanasojo@gmail.com>
2026-06-07 12:49:14 -0600

    Ejercicio 1, Prueba con prueba codigo

--- ejercicios/EvanUS/ejercicio01.py
+++ ejercicios/EvanUS/ejercicio01.py
@@ -1,3 +1,14 @@
 def sumar_digitos(numero):
-    if numero < 10:
-        return numero
+    if isinstance(numero, int) and numero >= 0:
+        return sumar_digitos_aux(numero)
+    else:
+        return 'Solo se admiten numeros enteros y positivos.'
+    
+def sumar_digitos_aux(numero,res=0):
+    if numero == 0:
+        return res
+    else:
+        res += numero % 10
+        return sumar_digitos_aux((numero // 10), res)
+    
+sumar_digitos(1234)
